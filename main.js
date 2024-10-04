c185077418dee7c16eb231cce69671b1e88b3740
Olah Aron Daniel <olah.aron@BOLYAI.LOCAL>
2024-10-04 12:36:06 +0200

    add form

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
-const array = [
+let array = [
     {
         firstname1: 'Géza',
         firstname2: 'Ferenc',
-        lastname: 'Kocsis'
+        lastname: 'Kocsis',
+        married: true,
+        pet: 'kutya'
     },
     {
         firstname1: 'Mária',
         firstname2: 'Júlia',
-        lastname: 'Horváth'
+        lastname: 'Horváth',
+        married: false,
+        pet: 'macska'
     },
     {
         firstname1: 'Ferenc',
-        lastname: 'Balogh'
+        lastname: 'Balogh',
+        married: false,
+        pet: 'teknős'
     },
     {
         firstname1: 'Gábor',
         firstname2: 'Attila',
-        lastname: 'Horváth'
+        lastname: 'Horváth',
+        married: true,
+        pet: 'macska'
     },
 ]
 let table = document.createElement('table')
@@ -25,9 +33,20 @@ let tablethaed = document.createElement("thead")
 table.appendChild(tablethaed)
 let tablerow = document.createElement("tr")
 tablethaed.appendChild(tablerow)
-let th = document.createElement("th")
-tablerow.appendChild(th)
-th.innerHTML = "Sándor"
+let nevek = ["Elso nev", "Masodik nev","Hazas","Állat"]
+let th1 = document.createElement("th")
+tablerow.appendChild(th1)
+th1.innerHTML = "Első név"
+let th2 = document.createElement("th")
+tablerow.appendChild(th2)
+th2.innerHTML = "Másodki név"
+th2.colSpan = 2
+let th3 = document.createElement("th")
+tablerow.appendChild(th3)
+th3.innerHTML = "Házas"
+let th4 = document.createElement("th")
+tablerow.appendChild(th4)
+th4.innerHTML = "Állat"
 let tbody = document.createElement("tbody")
 table.appendChild(tbody)
 for (let person of array) {
@@ -35,21 +54,28 @@ for (let person of array) {
     tbody.appendChild(tr)
     const lastname = document.createElement("td")
     tr.appendChild(lastname)
-    tablerow.appendChild(th)
     lastname.innerHTML = person.lastname
     const firstname = document.createElement("td")
     tr.appendChild(firstname)
-    tablerow.appendChild(th)
     firstname.innerHTML = person.firstname1
     if (person.firstname2 != undefined){
         const firstname2 = document.createElement("td")
         tr.appendChild(firstname2)
-        tablerow.appendChild(th)
         firstname2.innerHTML = person.firstname2
     }
     else{
         firstname.colSpan = 2
     }
+    //hazas start//
+    const hazas = document.createElement("td")
+    tr.appendChild(hazas)
+    if (person.married){ hazas.innerHTML = "✅"} else {hazas.innerHTML = "❌"}
+    //hazas end//
+    //allat start//
+    const allat = document.createElement("td")
+    tr.appendChild(allat)
+    allat.innerHTML = person.pet
+    //allat end//
     tr.addEventListener("click", function(e){
         console.log("click")
         const selected = tbody.querySelector(".selected")
